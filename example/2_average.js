649dd8e306d49c30a66de4f4add4230b3f81925c
hochan222 <201502766@hufs.ac.kr>
2018-04-29 23:21:17 +0900

    Add files via upload

---
+++ example/2_average.js
@@ -0,0 +1,14 @@
+﻿/*	함수를 완성해서 매개변수 array의 평균값을 return하도록 만들어 보세요.
+	어떠한 크기의 array가 와도 평균값을 구할 수 있어야 합니다.*/
+
+
+function average(array){
+  let sum = 0;
+  for(var i=0;i<array.length;i++){
+  sum += parseInt(array[i]);  
+  }
+  return sum;
+}
+
+var testArray = [5,3,4] 
+console.log("평균값 : " + average(testArray));
