3738f4c13f5fe3d067f3e1b071218a0a28340a97
hochan222 <201502766@hufs.ac.kr>
2018-04-30 23:00:07 +0900

    Add files via upload

---
+++ example/5_backStar.js
@@ -0,0 +1,17 @@
+﻿<script>
+function printReversedTriangle(num) {
+  var str = "";
+  
+  for(var i=num; i>0; i--){   
+    for(var j=i; j>0; j--){
+     	str += "*";
+    }
+      str += "\n";
+  }
+  return str;
+}
+
+
+// 아래는 테스트로 출력해 보기 위한 코드입니다.
+console.log("결과 : " +'\n'+ printReversedTriangle(4));
+</script>
