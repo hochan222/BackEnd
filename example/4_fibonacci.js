50ae1456ed02faf243270d02ddfb31d10da5239f
hochan222 <201502766@hufs.ac.kr>
2018-04-30 22:28:18 +0900

    Add files via upload

---
+++ example/4_fibonacci.js
@@ -0,0 +1,16 @@
+﻿<script>
+function fibonacci(num) {  
+  if(num === 0){
+  	return 0; 
+  }else if(num === 1){
+   	return 1; 
+  }else{
+   	return fibonacci(num-1) + fibonacci(num-2);
+  }
+}
+
+// 아래는 테스트로 출력해 보기 위한 코드입니다.
+console.log(fibonacci(3));
+console.log(fibonacci(4));
+console.log(fibonacci(10));
+</script>
