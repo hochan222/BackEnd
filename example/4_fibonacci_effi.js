8dbdab283d445020ff6f1c1feb5156483ae13843
hochan222 <201502766@hufs.ac.kr>
2018-04-30 22:45:40 +0900

    Add files via upload

---
+++ example/4_fibonacci_effi.js
@@ -0,0 +1,18 @@
+﻿<script>
+function fibonacci(num) {  
+  var a=0, b=1, temp;
+  
+  for(var i=1; i<num; i++){
+   	temp = a+b;
+    a = b; 
+    b = temp;
+     
+     console.log(a + " " +b);
+     console.log(" ");
+  }
+  return b;
+}
+
+// 아래는 테스트로 출력해 보기 위한 코드입니다.
+console.log(fibonacci(3));
+</script>
