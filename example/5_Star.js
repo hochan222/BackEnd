8a60aa69e8b5a9580734e4a82a9f69248af6527a
hochan222 <201502766@hufs.ac.kr>
2018-04-30 23:03:42 +0900

    Add files via upload

---
+++ example/5_Star.js
@@ -0,0 +1,17 @@
+﻿<script>
+function printTriangle(num) {
+  var str = "";
+  
+  for(var i=1; i<num+1; i++){   
+    for(var j=1; j<=i; j++){
+     	str += "*";
+    }
+      str += "\n";
+  }
+  return str;
+}
+
+
+// 아래는 테스트로 출력해 보기 위한 코드입니다.
+console.log( printTriangle(3) );
+</script>
