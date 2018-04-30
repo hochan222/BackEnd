cf3a8d57e1ef2e3f4765858b00865c721743ceaa
hochan222 <201502766@hufs.ac.kr>
2018-04-30 23:46:55 +0900

    Add files via upload

---
+++ example/6_string_number.js
@@ -0,0 +1,23 @@
+﻿<script>
+function alpha_string46(s){
+  var length = s.length;
+  var str = parseInt(s);
+  
+  var str2 = Number.isInteger(str);
+  console.log(str2);
+  
+  if(isNaN(str)){
+    return false;
+  }else if (length == 4){
+    return true;
+  }else if (length == 6){
+    return true;
+  }else 
+    return false;
+}
+
+
+// 아래는 테스트로 출력해 보기 위한 코드입니다.
+console.log( alpha_string46("a234") );
+console.log( alpha_string46("1234") );
+</script>
