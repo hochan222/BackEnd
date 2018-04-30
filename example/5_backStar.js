11e7cfac1f8182cbae1eb2a1d9486552388f89b3
hochan222 <201502766@hufs.ac.kr>
2018-04-30 14:52:54 +0000

    Update 5_backStar.js

--- example/5_backStar.js
+++ example/5_backStar.js
@@ -1,4 +1,4 @@
-﻿<script>
+
 function printReversedTriangle(num) {
   var str = "";
   
@@ -14,4 +14,4 @@ function printReversedTriangle(num) {
 
 // 아래는 테스트로 출력해 보기 위한 코드입니다.
 console.log("결과 : " +'\n'+ printReversedTriangle(4));
-</script>
+
