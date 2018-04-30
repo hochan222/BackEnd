bdff93f99fe8d68143b06328ad63de47e83cdb39
hochan222 <201502766@hufs.ac.kr>
2018-04-30 14:53:17 +0000

    Update 4_fibonacci_effi.js

--- example/4_fibonacci_effi.js
+++ example/4_fibonacci_effi.js
@@ -1,4 +1,4 @@
-﻿<script>
+
 function fibonacci(num) {  
   var a=0, b=1, temp;
   
@@ -15,4 +15,4 @@ function fibonacci(num) {
 
 // 아래는 테스트로 출력해 보기 위한 코드입니다.
 console.log(fibonacci(3));
-</script>
+
